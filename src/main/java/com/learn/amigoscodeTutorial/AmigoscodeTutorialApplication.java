4bb4a8ecc8d54a10454d01d92da134204a850a56
alprsln <alpars44@hotmail.com>
2021-08-20 18:30:25 +0300

    Initial commit

---
+++ src/main/java/com/learn/amigoscodeTutorial/AmigoscodeTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.learn.amigoscodeTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodeTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/amigoscodeTutorial/AmigoscodeTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.amigoscodeTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
