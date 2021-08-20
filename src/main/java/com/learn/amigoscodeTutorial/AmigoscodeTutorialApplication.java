635d86492ffdfe250e9c86fcd964d5ff36187ff5
alprsln <alpars44@hotmail.com>
2021-08-20 20:46:15 +0300

    feat!: add controller

--- src/main/java/com/learn/amigoscodeTutorial/AmigoscodeTutorialApplication.java
+++ src/main/java/com/learn/amigoscodeTutorial/AmigoscodeTutorialApplication.java
@@ -1,14 +1,7 @@
 package com.learn.amigoscodeTutorial;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
-import com.learn.amigoscodeTutorial.student.Student;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,19 +10,6 @@ public class AmigoscodeTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmigoscodeTutorialApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-			new Student(
-				1L,
-				"Alp",
-				"alpa@mail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-		);
 	}
 
 }
