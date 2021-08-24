cc1998fe273f7038a67f47c5c7d6f9504f05381c
alprsln <alpars44@hotmail.com>
2021-08-24 17:07:42 +0300

    feat!: add student registry with error ...
    ...handling

--- src/main/java/com/learn/amigoscode/student/StudentService.java
+++ src/main/java/com/learn/amigoscode/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.learn.amigoscode.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,14 @@ public class StudentService {
     public List<Student> getStudents(){
         return studentRepository.findAll();
     }
+
+	public void addNewStudent(Student student) {
+		Optional<Student> studentOptional = studentRepository
+			.findStudentByEmail(student.getEmail());
+		if(studentOptional.isPresent()) {
+			throw new IllegalStateException("email taken");
+		}
+		studentRepository.save(student);
+	}
 
 }
