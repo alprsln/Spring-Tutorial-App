37d58187721d1bf33cc1c25b1bbbf3297d1b7192
alprsln <alpars44@hotmail.com>
2021-08-25 20:12:19 +0300

    feat: add updateStudent methods

--- src/main/java/com/learn/amigoscode/student/StudentService.java
+++ src/main/java/com/learn/amigoscode/student/StudentService.java
@@ -1,7 +1,10 @@
 package com.learn.amigoscode.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
@@ -36,4 +39,26 @@ public class StudentService {
 		studentRepository.deleteById(studentId);
     }
 
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email){
+		Student student = studentRepository.findById(studentId)
+			.orElseThrow(() -> new IllegalStateException(
+				"student with id" + studentId + " does not exist"));
+		
+		if(name != null && 
+				name.length() > 0 &&
+				!Objects.equals(student.getName(), name))
+			student.setName(name);
+
+		if(email != null && 
+				email.length() > 0 &&
+				!Objects.equals(student.getEmail(), email)){
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+			if(studentOptional.isPresent()){
+				throw new IllegalStateException("email taken");
+			}
+			student.setEmail(email);
+		}
+	}
+	
 }
